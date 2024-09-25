6c573d215be91cd4f88fe39b70103a068a39fbbe
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2024-09-25 03:58:25 -0700

    Refactor handle_message function to include separate methods for Mandalorian seasons 1, 2, and 3

--- mtvserverutils.py
+++ mtvserverutils.py
@@ -558,8 +558,18 @@ class Media:
         self.cursor.execute(command)
         return self._fetch_all_as_dict()
     
-    def mandalorian(self):
-        command = "SELECT * FROM tvshows WHERE catagory='Mandalorian' ORDER BY Episode ASC;"
+    def mandalorians1(self):
+        command = "SELECT * FROM tvshows WHERE catagory='Mandalorian' AND season='01' ORDER BY Episode ASC;"
+        self.cursor.execute(command)
+        return self._fetch_all_as_dict()
+
+    def mandalorians2(self):
+        command = "SELECT * FROM tvshows WHERE catagory='Mandalorian' AND season='02' ORDER BY Episode ASC;"
+        self.cursor.execute(command)
+        return self._fetch_all_as_dict()
+    
+    def mandalorians3(self):
+        command = "SELECT * FROM tvshows WHERE catagory='Mandalorian' AND season='03' ORDER BY Episode ASC;"
         self.cursor.execute(command)
         return self._fetch_all_as_dict()
     
