65c46b2d7a050cc8874714dcbf7720c1a8cfa648
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-03-13 14:29:25 -0700

    Log video directory paths in Main class during processing

--- main.py
+++ main.py
@@ -54,6 +54,7 @@ class Main:
             mtvmovies.ProcessMovies(movs, self.conn, self.cursor).process()
 
             videos = utils.mtv_walk_dirs(os.getenv("MTV_VIDEOS_PATH"))
+            print(videos)
             mtvvideos.ProcessVideos(videos, self.conn, self.cursor).process()
 
         except sqlite3.OperationalError as e:
