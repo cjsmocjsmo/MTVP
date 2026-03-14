528203498ae7bf0c62c897c0b48791e9a61056c0
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-03-13 22:57:40 -0700

    Refactor video processing logic in ProcessVideos class to remove redundant regex checks and streamline insertion flow

--- mtvvideos.py
+++ mtvvideos.py
@@ -19,11 +19,6 @@ class ProcessVideos:
         self.conn = conn
         self.cursor = cursor
         self.vidlist = vids
-        self.vid = re.compile(r'VIDEO')
-        self.avi = re.compile(r'AVI')
-        self.mp4 = re.compile(r'MP4')
-        self.mpg = re.compile(r'MPG')
-        self.dcvid = re.compile(r'dcamVids')
         print(f"Initialized ProcessVideos with {len(vids)} videos.")
 
     def vid_id(self, file_path):
@@ -47,23 +42,19 @@ class ProcessVideos:
         print(f"Processing {len(self.vidlist)} videos...")
         for idx, v in enumerate(self.vidlist):
             print(v)
-            if self.vid.search(v) or self.avi.search(v) or self.mp4.search(v) or self.mpg.search(v) or self.dcvid.search(v):
-                try:
-                    vid_id = self.vid_id(v)
-                    name = self.vid_name(v)
-                    size = self.vid_size(v)
-                    idx += 1
-
-                    self.cursor.execute("SELECT * FROM videos WHERE VidId = ?", (vid_id,))
-                    existing_vid = self.cursor.fetchone()
-
-                    if existing_vid:
-                        logging.info(f"Video already exists in database: {name}")
-                        continue
-
-                    self.cursor.execute("INSERT INTO videos (VidId, VidPath, Size, Name, Idx) VALUES (?, ?, ?, ?, ?)", (vid_id, v, size, name, idx))
-                    self.conn.commit()
-                    print(f"Inserted video into database: {name}")
-                    logging.info(f"Inserted video into database: {name}")
-                except sqlite3.Error as e:
-                    logging.error(f"SQLite error while processing video {name}: {e}")
+            try:
+                vid_id = self.vid_id(v)
+                name = self.vid_name(v)
+                size = self.vid_size(v)
+                idx += 1
+                self.cursor.execute("SELECT * FROM videos WHERE VidId = ?", (vid_id,))
+                existing_vid = self.cursor.fetchone()
+                if existing_vid:
+                    logging.info(f"Video already exists in database: {name}")
+                    continue
+                self.cursor.execute("INSERT INTO videos (VidId, VidPath, Size, Name, Idx) VALUES (?, ?, ?, ?, ?)", (vid_id, v, size, name, idx))
+                self.conn.commit()
+                print(f"Inserted video into database: {name}")
+                logging.info(f"Inserted video into database: {name}")
+            except sqlite3.Error as e:
+                logging.error(f"SQLite error while processing video {name}: {e}")
