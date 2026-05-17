3cb594fad386e8cfad3e2ee1e9dfe38ecb4cb1d6
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2026-05-16 18:53:00 -0700

    feat: Implement category extraction and HTTP thumbnail path generation in InsertMovies function

--- go/setup/media.go
+++ go/setup/media.go
@@ -72,6 +72,134 @@ func GetMovieYear(filename string) string {
 	return ""
 }
 
+func GetMovieCategory(filename string) string {
+	catagory := ""
+	if strings.Contains(filename, "Action") {
+		catagory = "Action"
+	} else if strings.Contains(filename, "Arnold") {
+		catagory = "Arnold"
+	} else if strings.Contains(filename, "Avatar") {
+		catagory = "Avatar"
+	} else if strings.Contains(filename, "BruceLee") {
+		catagory = "BruceLee"
+	} else if strings.Contains(filename, "BruceWillis") {
+		catagory = "BruceWillis"
+	} else if strings.Contains(filename, "Buzz") {
+		catagory = "Buzz"
+	} else if strings.Contains(filename, "Cartoons") {
+		catagory = "Cartoons"
+	} else if strings.Contains(filename, "CharlieBrown") {
+		catagory = "CharlieBrown"
+	} else if strings.Contains(filename, "CheechAndChong") {
+		catagory = "CheechAndChong"
+	} else if strings.Contains(filename, "ChuckNorris") {
+		catagory = "ChuckNorris"
+	} else if strings.Contains(filename, "ClintEastwood") {
+		catagory = "ClintEastwood"
+	} else if strings.Contains(filename, "Comedy") {
+		catagory = "Comedy"
+	} else if strings.Contains(filename, "Documentary") {
+		catagory = "Documentary"
+	} else if strings.Contains(filename, "Drama") {
+		catagory = "Drama"
+	} else if strings.Contains(filename, "Fantasy") {
+		catagory = "Fantasy"
+	} else if strings.Contains(filename, "GhostBusters") {
+		catagory = "GhostBusters"
+	} else if strings.Contains(filename, "Godzilla") {
+		catagory = "Godzilla"
+	} else if strings.Contains(filename, "HarrisonFord") {
+		catagory = "HarrisonFord"
+	} else if strings.Contains(filename, "HarryPotter") {
+		catagory = "HarryPotter"
+	} else if strings.Contains(filename, "HellBoy") {
+		catagory = "HellBoy"
+	} else if strings.Contains(filename, "HomeVids") {
+		catagory = "HomeVids"
+	} else if strings.Contains(filename, "IndianaJones") {
+		catagory = "IndianaJones"
+	} else if strings.Contains(filename, "JamesBond") {
+		catagory = "JamesBond"
+	} else if strings.Contains(filename, "JohnWayne") {
+		catagory = "JohnWayne"
+	} else if strings.Contains(filename, "JohnWick") {
+		catagory = "JohnWick"
+	} else if strings.Contains(filename, "JurassicPark") {
+		catagory = "JurassicPark"
+	} else if strings.Contains(filename, "KevinCostner") {
+		catagory = "KevinCostner"
+	} else if strings.Contains(filename, "KingsMan") {
+		catagory = "KingsMan"
+	} else if strings.Contains(filename, "Lego") {
+		catagory = "Lego"
+	} else if strings.Contains(filename, "MenInBlack") {
+		catagory = "MenInBlack"
+	} else if strings.Contains(filename, "Minions") {
+		catagory = "Minions"
+	} else if strings.Contains(filename, "Misc") {
+		catagory = "Misc"
+	} else if strings.Contains(filename, "Mummy") {
+		catagory = "Mummy"
+	} else if strings.Contains(filename, "MusicVids") {
+		catagory = "MusicVids"
+	} else if strings.Contains(filename, "Nature") {
+		catagory = "Nature"
+	} else if strings.Contains(filename, "NicolasCage") {
+		catagory = "NicolasCage"
+	} else if strings.Contains(filename, "Oldies") {
+		catagory = "Oldies"
+	} else if strings.Contains(filename, "Pandas") {
+		catagory = "Pandas"
+	} else if strings.Contains(filename, "Pirates") {
+		catagory = "Pirates"
+	} else if strings.Contains(filename, "Predator") {
+		catagory = "Predator"
+	} else if strings.Contains(filename, "Riddick") {
+		catagory = "Riddick"
+	} else if strings.Contains(filename, "SciFi") {
+		catagory = "SciFi"
+	} else if strings.Contains(filename, "Science") {
+		catagory = "Science"
+	} else if strings.Contains(filename, "StarWars") {
+		catagory = "StarWars"
+	} else if strings.Contains(filename, "Stalone") {
+		catagory = "Stalone"
+	} else if strings.Contains(filename, "StarTrek") {
+		catagory = "StarTrek"
+	} else if strings.Contains(filename, "Stooges") {
+		catagory = "Stooges"
+	} else if strings.Contains(filename, "SuperHeros") {
+		catagory = "SuperHeros"
+	} else if strings.Contains(filename, "Superman") {
+		catagory = "Superman"
+	} else if strings.Contains(filename, "TheRock") {
+		catagory = "TheRock"
+	} else if strings.Contains(filename, "TinkerBell") {
+		catagory = "TinkerBell"
+	} else if strings.Contains(filename, "TomCruise") {
+		catagory = "TomCruise"
+	} else if strings.Contains(filename, "Tremors") {
+		catagory = "Tremors"
+	} else if strings.Contains(filename, "Transformers") {
+		catagory = "Transformers"
+	} else if strings.Contains(filename, "Trolls") {
+		catagory = "Trolls"
+	} else if strings.Contains(filename, "VanDam") {
+		catagory = "VanDam"
+	} else if strings.Contains(filename, "XMen") {
+		catagory = "XMen"
+	}
+	return catagory
+}
+
+func GetHttpThumbPath(filename string) string {
+	servAddr := os.Getenv("MTVGO_SERVER_ADDR")
+	port := os.Getenv("MTVGO_SERVER_PORT")
+	fn := filepath.Base(filename)
+	httpThumbPath := servAddr + ":" + port + "/thumbnails/" + fn + ".jpg"
+	return httpThumbPath
+}
+
 // InsertMovies inserts movie metadata into the database
 func InsertMovies(db *sql.DB, moviePaths []string, idxStart int) error {
 	for idx, path := range moviePaths {
@@ -87,9 +215,9 @@ func InsertMovies(db *sql.DB, moviePaths []string, idxStart int) error {
 		size := fileInfo.Size()
 		poster := strings.TrimSuffix(path, filepath.Ext(path)) + ".jpg"
 		fmt.Println("Poster path:", poster)
-		category := "" // TODO: implement category logic as in Python
-		thumbPath := "" // TODO: implement HTTP thumb path logic
-		_, err = db.Exec(`INSERT OR IGNORE INTO movies (Name, Year, PosterAddr, Size, Path, Idx, MovId, Catagory, HttpThumbPath) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+		category := GetMovieCategory(filename)
+		thumbPath := GetHttpThumbPath(filename)
+		_, err = db.Exec(`INSERT OR IGNORE INTO movies (Name, Year, PosterAddr, Size, Path, Idx, MovId, Category, HttpThumbPath) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
 			name, year, poster, size, path, idx+idxStart+1, movId, category, thumbPath)
 		if err != nil {
 			return fmt.Errorf("failed to insert movie %s: %w", path, err)
